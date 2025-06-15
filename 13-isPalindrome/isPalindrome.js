3412501c851f3ce57e7b282e9cf754c1af650d1f
Guarionex Tavares <guarotav@gmail.com>
2025-06-15 16:41:50 -0400

    Complete 13 code

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,13 @@
 function isPalindrome(word) {
   // Insert code here;
+  let reverseWord =''
+  for (i = word.length - 1; i >= 0 ; i--){
+    reverseWord+= word[i]
+  }
+  if (reverseWord == word)
+    return true;
+  else
+    return false;
 }
 
 // Do not edit this line;
