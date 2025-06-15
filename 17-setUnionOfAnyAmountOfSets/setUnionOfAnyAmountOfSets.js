2fc8ca85fb77bdac1f8f1bf8395e6a761059025e
Guarionex Tavares <guarotav@gmail.com>
2025-06-15 17:43:55 -0400

    Complete 17 code

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,13 @@
 function setUnionOfAnyAmountOfSets(...args) {
   // Insert code here;
+  let result = new Set()
+
+  for (let set of args) {
+    for (let value of set) {
+      result.add(value);
+    }
+  }
+    return result
 }
 
 // Do not edit this line;
