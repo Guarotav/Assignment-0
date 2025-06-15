07a64ddd5914f0f985e49a65269b389b53dd9c78
Guarionex Tavares <guarotav@gmail.com>
2025-06-15 16:18:35 -0400

    Complete 11 code

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,19 @@
 function sumOfMinimumAndMaximum(nums) {
   // Insert code here;
+  sum = 0
+  min = nums[0]
+  for (let i = 0; i < nums.length; i++){
+    if (nums [i] < min )
+    min = nums[i]
+  }
+  max = nums[0]
+  for (let i = 0; i < nums.length; i++){
+    if (nums [i] > max)
+    max = nums[i]
+  }
+  sum = min + max
+  return sum;
+
 }
 
 // Do not edit this line;
