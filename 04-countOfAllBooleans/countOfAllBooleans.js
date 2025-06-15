ac90bb4000a113a2d8b534402ded422cea94748e
Guarionex Tavares <guarotav@gmail.com>
2025-06-15 15:27:35 -0400

    Complete 04 code

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,6 +1,16 @@
 function countOfAllBooleans(arr) {
   // Insert code here;
+  let count = 0;
+  for (let i = 0; i < arr.length; i++){
+    if ( typeof arr[i] == 'boolean' ) {
+      count++;
+    }
+  }
+  return count;
 }
+
+console.log(countOfAllBooleans([true, false, 1, 'hello', true, 42]))
+
 
 // Do not edit this line;
 module.exports = countOfAllBooleans;
