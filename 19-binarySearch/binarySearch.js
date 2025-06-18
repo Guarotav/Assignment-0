0cbcc32048f3fcca9b29011f5324ba30c5294ed6
Guarotav <guarotav@gmail.com>
2025-06-18 16:43:33 -0400

    Completed 19 code

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -5,6 +5,20 @@ class MySolution {
 
   binarySearch(nums, target) {
     // Insert code here;
+    if (nums.length === 0) return false;
+
+    const mid = Math.floor(nums.length / 2);
+
+    if (nums[mid] === target) return true;
+    
+   
+    if (target < nums[mid]) {
+      
+      return this.binarySearch(nums.slice(0, mid), target);
+    } else {
+    
+      return this.binarySearch(nums.slice(mid + 1), target);
+    }
   }
 }
 
